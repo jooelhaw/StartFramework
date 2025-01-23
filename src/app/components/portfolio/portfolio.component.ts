13c74dc2279afc23c8bb4c17cdb2520ea2b92e1d
Youssef Elhaw <jooelhaw@gmail.com>
2025-01-23 02:08:08 +0200

    complete portfolio section and solve footer space between to space around (social icons)

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  TemplateRef,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { TitleComponent } from '../title/title.component';
 
 @Component({
@@ -8,10 +15,17 @@ import { TitleComponent } from '../title/title.component';
   styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent {
-  selectedImage: string | null = null;
-  onImageClick(src: string) {
-    this.selectedImage =src;
-    console.log(this.selectedImage);
-    
+  @ViewChild('selectedImg') selectedImg!: ElementRef;
+  @ViewChild('full') fullPage!: ElementRef;
+  // @ViewChildren('img') images!: QueryList<ElementRef>;
+
+  onClick(imgSrc: string) {
+    this.fullPage.nativeElement.style.display = 'flex';
+    this.selectedImg.nativeElement.src = imgSrc;
+  }
+  logDiv(event: MouseEvent, fullPage: HTMLDivElement) {
+    if (event.target == fullPage) {
+      fullPage.style.display = 'none';
+    }
   }
 }
