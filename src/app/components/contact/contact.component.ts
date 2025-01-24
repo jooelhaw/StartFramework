5a518f0f8474577b2cecea377bed1d379ddffba9
Youssef Elhaw <jooelhaw@gmail.com>
2025-01-24 05:12:01 +0200

    finished the app

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { TitleComponent } from '../title/title.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-
+  viewAge(userAge: HTMLLabelElement, input: HTMLInputElement) {
+    if (input.value === '') {
+      userAge.classList.add('label-site');
+    } else {
+      userAge.classList.remove('label-site');
+    }
+  }
 }
